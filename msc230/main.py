c454389164b85048a071b1c252c4ff9ada5b5e14
mshvn <shuravin@gmail.com>
2024-04-28 23:20:43 +0300

    black

--- msc230/main.py
+++ msc230/main.py
@@ -1,79 +1,71 @@
-# import numpy as np
-import os
 import argparse
-# import pickle
+import os
 
-# from sklearn.preprocessing import MinMaxScaler
-# import matplotlib.pyplot as plt
-
-# from tqdm import tqdm
-# import fnmatch
-
-# from PIL import Image as im
-# import cv2
-
-# from skimage.metrics import mean_squared_error as mse
-# from skimage.metrics import structural_similarity as ssim
-# from skimage.metrics import peak_signal_noise_ratio as psnr
-
+from process import inverse_transform_to_txt, process_matlab_txt, processed_to_png
 from utils import get_metrics, plot_polar
-from process import process_matlab_txt, processed_to_png, inverse_transform_to_txt
+
 
 def main(action) -> None:
-    '''
+    """
     Computer Vision tools for MSC230 Super-Resolution project.
-    
+
     Basic usage:
     * * *
-    
-    '''
+
+    """
     # add ascii fancy header
-    print('\
+    print(
+        "\
     ████████████████████████████████████\n\
     █▄─▀█▀─▄█─▄▄▄▄█─▄▄▄─█▀▄▄▀█▄▄▄░█─▄▄─█\n\
     ██─█▄█─██▄▄▄▄─█─███▀██▀▄███▄▄░█─██─█\n\
-    ▀▄▄▄▀▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▀▄▄▄▄▀▄▄▄▄▀')
+    ▀▄▄▄▀▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▀▄▄▄▄▀▄▄▄▄▀"
+    )
 
     root_path = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
-    print('\nRoot folder path:', root_path)
+    print("\nRoot folder path:", root_path)
 
-    print('Selected action:', action)
+    print("Selected action:", action)
 
     # models_path = 'models/'
     # / added in end (?):
-    raw_path = 'data/raw/'
-    processed_path = 'data/processed/'
-    png_path = 'data/png/'
-    scalers_path = 'data/scalers/'
-    results_path = 'results/'
-    output_path = 'output/'
-    polar_path = 'polar/'
+    raw_path = "data/raw/"
+    processed_path = "data/processed/"
+    png_path = "data/png/"
+    scalers_path = "data/scalers/"
+    results_path = "results/"
+    output_path = "output/"
+    polar_path = "polar/"
 
     action = action.lower()
 
-    if action in ['process_matlab', 'process_mat', 'process', 'pm']:
+    if action in ["process_matlab", "process_mat", "process", "pm"]:
         process_matlab_txt(root_path, raw_path, processed_path)
-    elif action in ['to_png', 'png', 'top', 'tp']:
+    elif action in ["to_png", "png", "top", "tp"]:
         processed_to_png(root_path, processed_path, scalers_path, png_path)
-    elif action in ['inverse_transform', 'inv_trans', 'invtrans','invt', 'it']:
-        inverse_transform_to_txt(root_path, png_path, results_path, output_path, scalers_path)
-    elif action in ['metrics', 'metric', 'met', 'ms']:
+    elif action in ["inverse_transform", "inv_trans", "invtrans", "invt", "it"]:
+        inverse_transform_to_txt(
+            root_path, png_path, results_path, output_path, scalers_path
+        )
+    elif action in ["metrics", "metric", "met", "ms"]:
         get_metrics(root_path, results_path, png_path)
-    elif action in ['plotpolar', 'plot', 'polar', 'pp']:
+    elif action in ["plotpolar", "plot", "polar", "pp"]:
         plot_polar(root_path, results_path, polar_path)
-    elif action in ['full', 'fullforward', 'forward', 'ff']:
+    elif action in ["full", "fullforward", "forward", "ff"]:
         process_matlab_txt(root_path, raw_path, processed_path)
         processed_to_png(root_path, processed_path, scalers_path, png_path)
     else:
-        print(f'Wrong action provided ({action}).')
-    
+        print(f"Wrong action provided ({action}).")
+
     return
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog = 'python msc230/main.py', 
-                                     description = 'Actions parser for MSC230 tools.\
-                                        Please enter action.')
-    parser.add_argument('action', help='valid actions: pm, tp, it, ms, pp, ff')
+    parser = argparse.ArgumentParser(
+        prog="python msc230/main.py",
+        description="Actions parser for MSC230 tools.\
+                                        Please enter action.",
+    )
+    parser.add_argument("action", help="valid actions: pm, tp, it, ms, pp, ff")
     args = parser.parse_args()
     main(args.action)
--- msc230/utils.py
+++ msc230/utils.py
@@ -1,60 +1,68 @@
-import numpy as np
+import fnmatch
 import os
 
-import matplotlib.pyplot as plt
-
-from tqdm import tqdm
-import fnmatch
-
-from PIL import Image as im
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image as im
 
 # from skimage.metrics import mean_squared_error as mse
-from skimage.metrics import structural_similarity as ssim
 from skimage.metrics import peak_signal_noise_ratio as psnr
-
+from skimage.metrics import structural_similarity as ssim
+from tqdm import tqdm
 
 
 def get_metrics(root_path, results_path, png_path) -> None:
-    '''
+    """
     Prints basic metrics (SSIM, PSNR).
 
     Args:
         root_path (str) : absolute path to root folder of the project
         results_path (str) : relative path to [results] folder
         png_path (str) : relative path to [png] folder
-    
+
     Returns:
         Nothing
 
-    '''
+    """
 
-    print('METRICS')
+    print("METRICS")
 
     results_list = sorted(os.listdir(os.path.join(root_path, results_path)))
-    results_list = fnmatch.filter(results_list, '*.png')
+    results_list = fnmatch.filter(results_list, "*.png")
 
-    print(*results_list, sep='\n')
-    print(f'Going to print SSIM and PSNR metrics for {len(results_list)} files in [{results_path}] folder.')
+    print(*results_list, sep="\n")
+    print(
+        f"Going to print SSIM and PSNR metrics for {len(results_list)} files in [{results_path}] folder."
+    )
 
-    user_input = input('Do you want to continue? (y/n): ')
-    if user_input.lower() not in ['yes', 'y', 'yep']:
+    user_input = input("Do you want to continue? (y/n): ")
+    if user_input.lower() not in ["yes", "y", "yep"]:
         return
-    
+
     for result in results_list:
 
-        print('\nMetrics for:', result)
-        number = result.split('_')[1]
+        print("\nMetrics for:", result)
+        number = result.split("_")[1]
 
-        ground_big = cv2.imread(os.path.join(root_path, png_path, f'ground_{number}.png'), cv2.IMREAD_GRAYSCALE)
-        upscaled_bic = cv2.imread(os.path.join(root_path, png_path, f'bicubic_{number}.png'), cv2.IMREAD_GRAYSCALE)
-        upscaled_nn = cv2.imread(os.path.join(root_path, results_path, f'compr_{number}_out.png'), cv2.IMREAD_GRAYSCALE)
+        ground_big = cv2.imread(
+            os.path.join(root_path, png_path, f"ground_{number}.png"),
+            cv2.IMREAD_GRAYSCALE,
+        )
+        upscaled_bic = cv2.imread(
+            os.path.join(root_path, png_path, f"bicubic_{number}.png"),
+            cv2.IMREAD_GRAYSCALE,
+        )
+        upscaled_nn = cv2.imread(
+            os.path.join(root_path, results_path, f"compr_{number}_out.png"),
+            cv2.IMREAD_GRAYSCALE,
+        )
 
-        print('SSIM BIC:', ssim(ground_big, upscaled_bic))
-        print('SSIM NN: ', ssim(ground_big, upscaled_nn))
+        print("SSIM BIC:", ssim(ground_big, upscaled_bic))
+        print("SSIM NN: ", ssim(ground_big, upscaled_nn))
 
-        print('PSNR BIC:', psnr(ground_big, upscaled_bic))
-        print('PSNR NN: ', psnr(ground_big, upscaled_nn))
+        print("PSNR BIC:", psnr(ground_big, upscaled_bic))
+        print("PSNR NN: ", psnr(ground_big, upscaled_nn))
 
         # print(mse(ground_big, upscaled_bic))
         # print(mse(ground_big, upscaled_nn))
@@ -63,7 +71,7 @@ def get_metrics(root_path, results_path, png_path) -> None:
 
 
 def plot_polar(root_path, results_path, polar_path) -> None:
-    '''
+    """
     Converts images from cartesian to polar.
     Saves images to [polar] folder.
 
@@ -71,26 +79,25 @@ def plot_polar(root_path, results_path, polar_path) -> None:
         root_path (str) : absolute path to root folder of the project
         results_path (str) : relative path to [results] folder
         polar_path (str) : relative path to [polar] folder
-    
+
     Returns:
         Nothing
 
-    '''
+    """
 
-    print('PLOT POLAR')
+    print("PLOT POLAR")
 
     # get list of files, filter PNG only
     results_list = sorted(os.listdir(os.path.join(root_path, results_path)))
-    results_list = fnmatch.filter(results_list, '*.png')
+    results_list = fnmatch.filter(results_list, "*.png")
 
-    print(*results_list, sep='\n')
-    print(f'Going to process {len(results_list)} PNG files in [{results_path}] folder.')
+    print(*results_list, sep="\n")
+    print(f"Going to process {len(results_list)} PNG files in [{results_path}] folder.")
 
-
-    user_input = input('Do you want to continue? (y/n): ')
-    if user_input.lower() not in ['yes', 'y', 'yep']:
+    user_input = input("Do you want to continue? (y/n): ")
+    if user_input.lower() not in ["yes", "y", "yep"]:
         return
-        
+
     else:
         for result in results_list:
             image = np.array(im.open(os.path.join(root_path, results_path, result)))
@@ -102,8 +109,9 @@ def plot_polar(root_path, results_path, polar_path) -> None:
             # print(picture.shape)
             # print(len(theta_steps), theta_steps[-5:])
 
-
-            for step in tqdm(range(len(image)), ncols=90, desc=f'Converting [{result}] '):
+            for step in tqdm(
+                range(len(image)), ncols=90, desc=f"Converting [{result}] "
+            ):
                 # print('THETA STEPS:', theta_steps[step])
                 # print(df.iloc[i])
                 r = 0
@@ -112,21 +120,27 @@ def plot_polar(root_path, results_path, polar_path) -> None:
                     # print(f'Value {val} in radius {r}, angle {angle_steps[step]}')
                     r = r + 1
 
-                    x = r * np.cos(np.deg2rad(theta_steps[step])) + 999#len(r_steps)-3#1999
-                    y = r * np.sin(np.deg2rad(theta_steps[step])) + 999#len(r_steps)-3#1999
+                    x = (
+                        r * np.cos(np.deg2rad(theta_steps[step])) + 999
+                    )  # len(r_steps)-3#1999
+                    y = (
+                        r * np.sin(np.deg2rad(theta_steps[step])) + 999
+                    )  # len(r_steps)-3#1999
 
                     # print('X Y:', x, y)
                     picture[round(x)][round(y)] = val
 
                     # if theta_steps[step] % 30 == 0:
-                    #     print('*', end='') 
-            
-            plt.figure(figsize=(20,20))
+                    #     print('*', end='')
+
+            plt.figure(figsize=(20, 20))
             plt.imshow(picture)
-            plt.savefig(os.path.join(root_path, polar_path, 'polar_figure_' + result))
+            plt.savefig(os.path.join(root_path, polar_path, "polar_figure_" + result))
 
-            out_imgage = im.fromarray(np.uint8(picture), 'L')
-            out_imgage.save(os.path.join(root_path, polar_path, 'polar_image_' + result))
+            out_imgage = im.fromarray(np.uint8(picture), "L")
+            out_imgage.save(
+                os.path.join(root_path, polar_path, "polar_image_" + result)
+            )
 
-        print('Done.')
+        print("Done.")
         return
